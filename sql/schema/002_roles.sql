0ecb502d9a1285de2ecdb47ca33d38981e5cd041
SSC <sergej.schweizer@gmail.com>
2026-08-22 22:03:37 +0200

    feat(xdl-pr008-postgres-role-grants): add writer and read-only grants

---
+++ sql/schema/002_roles.sql
@@ -0,0 +1,43 @@
+BEGIN;
+
+DO $$
+BEGIN
+    CREATE ROLE portfell_data_loader NOLOGIN;
+EXCEPTION
+    WHEN duplicate_object THEN NULL;
+END
+$$;
+
+DO $$
+BEGIN
+    CREATE ROLE portfell_app NOLOGIN;
+EXCEPTION
+    WHEN duplicate_object THEN NULL;
+END
+$$;
+
+REVOKE ALL ON SCHEMA portfell_market FROM PUBLIC;
+GRANT USAGE ON SCHEMA portfell_market TO portfell_data_loader, portfell_app;
+REVOKE CREATE ON SCHEMA portfell_market FROM portfell_data_loader, portfell_app;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA portfell_market
+    TO portfell_data_loader;
+GRANT SELECT ON ALL TABLES IN SCHEMA portfell_market TO portfell_app;
+REVOKE INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER
+    ON ALL TABLES IN SCHEMA portfell_market FROM portfell_app;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA portfell_market
+    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO portfell_data_loader;
+ALTER DEFAULT PRIVILEGES IN SCHEMA portfell_market
+    GRANT SELECT ON TABLES TO portfell_app;
+
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM pg_namespace WHERE nspname = 'portfell_loader_sync') THEN
+        REVOKE ALL ON SCHEMA portfell_loader_sync FROM portfell_app;
+        REVOKE ALL ON ALL TABLES IN SCHEMA portfell_loader_sync FROM portfell_app;
+    END IF;
+END
+$$;
+
+COMMIT;
